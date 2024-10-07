369321980ec9af7d5951502e151cbc00455b5af0
MuhammedA-maker <meme_abdo202@outlook.com>
2024-10-07 12:25:59 +0300

    Create variables.tf

---
+++ terraform_lab/variables.tf
@@ -0,0 +1,25 @@
+variable "region" {
+  type = string
+}
+
+variable "ami" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+variable "ec2_instance_type" {
+  type = string
+}
+
+variable "public_subnet_cidr" {
+  type = string
+}
+variable "private_subnet_cidr" {
+  type = string
+}
+
+
+
+
